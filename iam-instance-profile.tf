14bd36e68292f6a265e6cdc9d5f53d8246c82d82
Saurabh <saurabh16194@gmail.com>
2025-06-16 13:46:08 +0530

    update in iam role name

--- iam-instance-profile.tf
+++ iam-instance-profile.tf
@@ -1,5 +1,5 @@
 #IAM Instance Profile: Binds the role to an EC2 instance
 resource "aws_iam_instance_profile" "jenkins_profile" {
     name = "Jenkins_instance_profile"
-    role = "aws_iam_role.jenkins_role" 
+    role = aws_iam_role.jenkins_role.name
 }
